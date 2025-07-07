e64e2efd29ccad1e7798b0006f90710d99a5a329
ThanushaBasrireddygari <thanusha2005reddy@gmail.com>
2025-07-07 10:22:02 +0530

    Add files via upload
    wipro_talentnext_2025

---
+++ Firstprogram.java
@@ -0,0 +1,12 @@
+package wipro_talentnext;
+import java.util.Scanner;
+public class Firstprogram {
+	  public static void main(String args[])
+	  {
+		  String s1,s2;
+		  s1="ABC";
+		  s2="Mumbai";
+		  System.out.println(s1 + " Technologies " + s2);
+	  }
+
+}
